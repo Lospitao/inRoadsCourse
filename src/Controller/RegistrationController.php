7c6af929e4cfe6f2c171ca02b85704f33123d999
Laura LOspitao <lauralospitao@gmail.com>
2021-02-16 17:51:36 +0100

    #6 Registration in Database

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegistrationController extends AbstractController
+{
+    var $user;
+    var $form;
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        try {
+            $this->createNewUser();
+            $this->generateForm($request);
+            if ($this->form->isSubmitted() && $this->form->isValid()) {
+                $this->setSignupDate();
+                $this->setEncodedPassword($passwordEncoder);
+                $this->persistNewUserToDataBase();
+                $this->addFlash('success', 'Se ha registrado con éxito');
+                return $this->redirectToRoute('app_register');
+            }
+        } catch (\Doctrine\ORM\EntityNotFoundException $ex) {
+            error_log($ex->getMessage());
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $this->form->createView(),
+        ]);
+    }
+
+    private function createNewUser()
+    {
+        $this->user = new User();
+    }
+
+    private function generateForm($request)
+    {
+        $this->form = $this->createForm(RegistrationFormType::class, $this->user);
+        $this->form->handleRequest($request);
+    }
+
+    private function setSignupDate()
+    {
+        $this->user->setSignupDate(new \DateTime());
+    }
+
+    private function setEncodedPassword($passwordEncoder)
+    {
+        $this->user->setPassword(
+            $passwordEncoder->encodePassword(
+                $this->user,
+                $this->form->get('plainPassword')->getData()
+            )
+        );
+    }
+
+    private function persistNewUserToDataBase()
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($this->user);
+        $entityManager->flush();
+    }
+}
